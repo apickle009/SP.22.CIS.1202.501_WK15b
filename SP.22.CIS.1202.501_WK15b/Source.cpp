3f3c7d9d8ee2f026fdf0bddd7b7ade03f7260d2d
TonyP009 <apickle009@gmail.com>
2022-04-30 02:18:24 -0400

    updating code to include both templates. This is processing with error when trying to round.

--- SP.22.CIS.1202.501_WK15b/Source.cpp
+++ SP.22.CIS.1202.501_WK15b/Source.cpp
@@ -3,17 +3,37 @@
 //4.30.2022
 
 #include <iostream>
+#include <cmath>	
 
-using namespace std; 
+using namespace std;
 
+template <typename T>
+T half (T mush)
+{
+	T round;
+	round = mush / 2;
+	return round; 
+}
+
+template <typename T>
+T half(int mash)
+{
+	T round;
+	round = mash / 2;
+	round(static_cast mash < ); 
+	return round;
+
+}
 
 int main() {
 
+	double a = 7.0;
+	float b = 5.0f;
+	int c = 3; 
 
+	cout << half(a) << endl;
+	cout << half(b) << endl;
+	cout << half(c) << endl;
 
-
-
-
-
-	return 0; 
+	return 0;
 }
